8f3e1092e889ec820ed97638d60b7904e60e4d8d
hamdi <hamdi.babdelhafidh@gmail.com>
2022-07-05 21:50:39 +0200

    array types

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var person = {
     name: "hamdi",
-    age: 30
+    age: 30,
+    hobbies: ["sports", "Cooking"]
 };
 // error person type
+var favoriteActivites; // array of strings
+// errors =>  favoriteActivites = "sports" ||  ["sports",1]
+favoriteActivites = ["sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-const person: {
-    name: string,
-    age:number
-} = {
+const person = {
     name: "hamdi",
-    age:30
+    age: 30,
+    hobbies:["sports","Cooking"]
 }
 // error person type
+
+let favoriteActivites: string[];// array of strings
+// errors =>  favoriteActivites = "sports" ||  ["sports",1]
+
+favoriteActivites = ["sports"]
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
