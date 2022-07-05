077ace3fe532438689af395086516d57dbc8c364
hamdi <hamdi.babdelhafidh@gmail.com>
2022-07-05 22:45:54 +0200

    aliases types

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
-console.log(combinedAges); //correct
-var combinedNames = combine("ha", "md");
-console.log(combinedNames); //error 
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedAges2 = combine("30", "26", "as-number");
+console.log(combinedAges2);
+var combinedNames = combine("ha", "mdi", "as-string");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
-function combine(input1: number | string, input2: number | string) {
+// keybord type
+type Combinable = number | string
+type ConversionDesc = "as-number" | "as-string"
+
+
+function combine(
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion: ConversionDesc
+) {
 
     let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2
+    if (typeof input1 === "number" && typeof input2 === "number" ||resultConversion ==="as-number") {
+        result = +input1 +  +input2
     }else {
         result = input1.toString() + input2.toString()
     } 
     return result
 }
     
-const combinedAges = combine(30, 26)
-console.log(combinedAges) //correct
+const combinedAges = combine(30, 26,"as-number")
+console.log(combinedAges) 
 
-const combinedNames = combine("ha", "mdi")
-console.log(combinedNames) //error 
+
+const combinedAges2 = combine("30", "26","as-number")
+console.log(combinedAges2) 
+
+const combinedNames = combine("ha", "mdi","as-string")
+console.log(combinedNames)  
