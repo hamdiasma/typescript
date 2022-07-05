2aee8b534da2f5dd12f8c3512731bddf5f58d397
hamdi <hamdi.babdelhafidh@gmail.com>
2022-07-05 21:30:35 +0200

    basics ts

---
+++ basics.ts
@@ -0,0 +1,18 @@
+function add(n1: number, n2: number, printResult: boolean) {
+  
+  if (printResult) {
+   console.log(n1 + n2)
+  } else {
+    return n1 + n2
+  }
+  
+}
+
+
+const number1: number = 5
+const number2 = 2.8
+const printResult = true
+
+
+
+const result = add(number1, number2,printResult)
