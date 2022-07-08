040078d951ba6750cdcee456c4488c113a482d21
hamdi <hamdi.babdelhafidh@gmail.com>
2022-07-08 11:43:16 +0200

    unknown type

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+// unknown type
+var userInput;
+var userName;
+userInput = 5;
+userInput = "hi";
+// userName = userInput // Type 'unknown' is not assignable ~/Desktop/ts (partOne) to type 'string'.
+if (typeof userInput === "string") {
+    // with unknown type we need extra check
+    userName = userInput;
+    console.log(userName);
 }
-// white type
-// function printResult2(num:number):undefined { // error no undefined type for function
-//     console.log("Result: " + num)
-// }
-function printResult(num) {
-    console.log("Result: " + num);
-}
-// callback function type
-function addHandler(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-//function without retun any things === undefined
-printResult(add(10, 9)); // 19
-// let combineValues:Function; type Function
-var combineValues; //accept any nuber and return number 
-combineValues = add;
-// combineValues= 5 // error type function
-//combineValues = printResult // undefined
-console.log(combineValues(8, 8));
-// return undefined
-//console.log(printResult(add(10,9))) //undifined
-addHandler(10, 5, function (res) {
-    console.log("cb: ", res);
-});
--- app.ts
+++ app.ts
@@ -1,42 +1,18 @@
-function add(n1:number,n2:number) {
-    return n1 + n2
+// unknown type
+
+let userInput: unknown
+let userName: string
+
+userInput = 5;
+userInput = "hi"
+
+// userName = userInput // Type 'unknown' is not assignable ~/Desktop/ts (partOne) to type 'string'.
+
+if (typeof userInput === "string") {
+    // with unknown type we need extra check
+    userName = userInput
+
+    console.log(userName)
 }
 
-// white type
-// function printResult2(num:number):undefined { // error no undefined type for function
-//     console.log("Result: " + num)
-// }
 
-function printResult(num:number):void {
-    console.log("Result: " + num);
-}
-
-
-// callback function type
-
-function addHandler(n1: number, n2: number,cb:(num:number)=> void) {
-    const result = n1 + n2
-    cb(result)
-}
-
-//function without retun any things === undefined
-printResult(add(10, 9)) // 19
-
-// let combineValues:Function; type Function
-let combineValues:(a:number,b:number)=> number; //accept any nuber and return number 
-
-combineValues = add
-// combineValues= 5 // error type function
-//combineValues = printResult // undefined
-
-console.log(combineValues(8,8))
-
-// return undefined
-//console.log(printResult(add(10,9))) //undifined
-
-
-
-
-addHandler(10, 5, (res) => {
-    console.log("cb: ", res)
-})
